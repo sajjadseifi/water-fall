1d8bf684bbc6b31939949ba86d375389423ceb6d
Sajjad Seifi <s.09334497255@gmail.com>
2022-07-30 17:08:51 +0430

    first implimentation

---
+++ index.js
@@ -0,0 +1,27 @@
+const animationDuration = 4000 + 500
+const root = document.getElementById('root')
+const randomLeft = ()=> Math.random() * 80 + 10
+const rotate = ()=> {
+    const neg = Math.random() * 2 > 1 ? -1 : 1  
+    const r = Math.random() * 360
+
+    return neg * r
+}
+
+const removeFromDom = (node)=>{
+    root.removeChild(node)
+}
+document.addEventListener('keyup',function(event){
+    const { key } = event
+    const span = document.createElement('span')
+    span.innerHTML = key
+    span.className = 'keyword'
+
+    span.style.left = `${randomLeft()}%`
+    span.style.transform = `rotate(${rotate()}deg)`
+    root.appendChild(span)
+    
+    setTimeout(() => removeFromDom(span), animationDuration);
+})
+
+
